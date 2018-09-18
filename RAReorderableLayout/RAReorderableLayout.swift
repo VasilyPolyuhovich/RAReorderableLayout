811250aaf0acd1ff4edb66172d0b0b64cd4f00b4
Vasily Polyuhovich <vasilitch@gmail.com>
2018-09-18 15:18:32 +0300

    Changed Swift version

--- RAReorderableLayout/RAReorderableLayout.swift
+++ RAReorderableLayout/RAReorderableLayout.swift
@@ -110,7 +110,7 @@ open class RAReorderableLayout: UICollectionViewFlowLayout, UIGestureRecognizerD
     
     fileprivate var fakeCellCenter: CGPoint?
     
-    fileprivate var trigerInsets = UIEdgeInsetsMake(100.0, 100.0, 100.0, 100.0)
+    fileprivate var trigerInsets = UIEdgeInsets.init(top: 100.0, left: 100.0, bottom: 100.0, right: 100.0)
     
     fileprivate var trigerPadding = UIEdgeInsets.zero
     
@@ -238,7 +238,7 @@ open class RAReorderableLayout: UICollectionViewFlowLayout, UIGestureRecognizerD
         }
         
         displayLink = CADisplayLink(target: self, selector: #selector(RAReorderableLayout.continuousScroll))
-        displayLink!.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
+        displayLink!.add(to: RunLoop.main, forMode: RunLoop.Mode.common)
     }
     
     fileprivate func invalidateDisplayLink() {
@@ -524,11 +524,11 @@ private class RACellFakeView: UIView {
         layer.shouldRasterize = false
         
         cellFakeImageView = UIImageView(frame: self.bounds)
-        cellFakeImageView?.contentMode = UIViewContentMode.scaleAspectFill
+        cellFakeImageView?.contentMode = UIView.ContentMode.scaleAspectFill
         cellFakeImageView?.autoresizingMask = [.flexibleWidth , .flexibleHeight]
         
         cellFakeHightedView = UIImageView(frame: self.bounds)
-        cellFakeHightedView?.contentMode = UIViewContentMode.scaleAspectFill
+        cellFakeHightedView?.contentMode = UIView.ContentMode.scaleAspectFill
         cellFakeHightedView?.autoresizingMask = [.flexibleWidth , .flexibleHeight]
         
         cell.isHighlighted = true
@@ -567,7 +567,7 @@ private class RACellFakeView: UIView {
                 shadowAnimation.fromValue = 0
                 shadowAnimation.toValue = 0.7
                 shadowAnimation.isRemovedOnCompletion = false
-                shadowAnimation.fillMode = kCAFillModeForwards
+                shadowAnimation.fillMode = CAMediaTimingFillMode.forwards
                 self.layer.add(shadowAnimation, forKey: "applyShadow")
             },
             completion: { _ in
@@ -588,7 +588,7 @@ private class RACellFakeView: UIView {
                 shadowAnimation.fromValue = 0.7
                 shadowAnimation.toValue = 0
                 shadowAnimation.isRemovedOnCompletion = false
-                shadowAnimation.fillMode = kCAFillModeForwards
+                shadowAnimation.fillMode = CAMediaTimingFillMode.forwards
                 self.layer.add(shadowAnimation, forKey: "removeShadow")
             },
             completion: { _ in
